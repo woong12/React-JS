ee2345413c8fa28ded5b3231991c2858eb99df46
woong <103757515+woong12@users.noreply.github.com>
2022-10-20 22:57:25 +0900

    Add files via upload

--- React JS/src/App.js
+++ React JS/src/App.js
@@ -4,13 +4,26 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
     console.log("i run all the time");
     useEffect(() => {
         console.log("CALL THE API....");
     }, []);
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("SEARCH FOR", keyword);
+        }
+    }, [keyword]);
     return (
         <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here..."
+            ></input>
             <h1>{counter}</h1>
             <button onClick={onClick}>click me</button>
         </div>
