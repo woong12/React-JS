a1b01389e5c72652a7148be082075ce7cae02653
woong <103757515+woong12@users.noreply.github.com>
2022-10-20 23:37:49 +0900

    Add files via upload

--- React JS/src/App.js
+++ React JS/src/App.js
@@ -2,20 +2,53 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+    //1
+    /* function byeFn() {
+        console.log("bye :(");
+    }
+    function hiFn() {
+        console.log("created :)");
+        return byeFn;
+    }
+    useEffect(hiFn, []); */
+
+    //2
+    useEffect(function () {
+        console.log("hi :)");
+        return function () {
+            console.log("bye :(");
+        };
+    }, []);
+    return <h1>Hello</h1>;
+}
+
 function App() {
-    const [counter, setValue] = useState(0);
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
+        <div>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        </div>
+    );
+
+    /* const [counter, setValue] = useState(0);
     const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
     const onChange = (event) => setKeyword(event.target.value);
-    console.log("i run all the time");
     useEffect(() => {
-        console.log("CALL THE API....");
+        console.log("i run only once.");
     }, []);
     useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("SEARCH FOR", keyword);
-        }
+        console.log("I run when 'keyword' changes.");
     }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter]);
+    useEffect(() => {
+        console.log("I run when keyword & counter change");
+    });
     return (
         <div>
             <input
@@ -27,7 +60,7 @@ function App() {
             <h1>{counter}</h1>
             <button onClick={onClick}>click me</button>
         </div>
-    );
+    ); */
 }
 
 export default App;
